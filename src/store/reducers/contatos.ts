a24530a8d084ebd8d11e9737e1b22df5dd4aa881
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-08-28 23:36:28 -0300

    Iniciando com a lista vazia de contatos.

--- src/store/reducers/contatos.ts
+++ src/store/reducers/contatos.ts
@@ -1,35 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import Contato from '../../models/Contato';
-import * as enums from '../../utils/enums/EnumContatos';
 
 type ContatosState = {
   itens: Contato[];
 };
 
 const initialState: ContatosState = {
-  itens: [
-    {
-      id: 1,
-      nomeContato: 'Teste Teste Teste',
-      tagContato: enums.TagEnum.FAMILIA,
-      phoneContato: '48911111111',
-      emailContato: 'teste@teste.com',
-    },
-    {
-      id: 2,
-      nomeContato: 'Teste2 Teste2 Teste2',
-      tagContato: enums.TagEnum.AMIZADE,
-      phoneContato: '48922222222',
-      emailContato: 'teste2@teste2.com',
-    },
-    {
-      id: 3,
-      nomeContato: 'Teste3 Teste3 Teste3',
-      tagContato: enums.TagEnum.TRABALHO,
-      phoneContato: '48933333333',
-      emailContato: 'teste3@teste3.com',
-    },
-  ],
+  itens: [],
 };
 
 const contatoSlice = createSlice({
